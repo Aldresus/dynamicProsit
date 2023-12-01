a3e91ba38a802ab3996bf52ba0f47ef81173e5b4
Hugo Champy <hugo.champy@k-expert.com>
2023-12-01 11:22:00 +0100

    fixed og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,11 +71,17 @@ export default function RootLayout({
 			<head>
 				<title>DynamicPrositX</title>
 				<ColorSchemeScript />
-				<meta property="og:image" content="<generated>" />
-				<meta property="og:image:alt" content="About Acme" />
+				<meta property="og:image" content="/opengraph-image" />
+				<meta property="og:image:alt" content="DynamicPrositX" />
 				<meta property="og:image:type" content="image/png" />
 				<meta property="og:image:width" content="1200" />
 				<meta property="og:image:height" content="630" />
+
+				<meta name="twitter:image" content="/twitter-image" />
+				<meta name="twitter:image:alt" content="DynamicPrositX" />
+				<meta name="twitter:image:width" content="1200" />
+				<meta name="twitter:image:height" content="630" />
+
 			</head>
 			<body>
 				<MantineProvider defaultColorScheme="light" theme={theme}>
