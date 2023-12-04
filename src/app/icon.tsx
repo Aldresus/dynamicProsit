1700812c51a034bb8498916629c8d9f409ffe875
Aldresus <aldresusgames@gmail.com>
2023-12-04 09:50:52 +0100

    fixed favicon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -33,7 +33,13 @@ export default function Icon() {
 				strokeLinecap="round"
 				strokeLinejoin="round"
 			>
-				<title>favicon</title>
+				<title
+					style={{
+						display: "none",
+					}}
+				>
+					favicon
+				</title>
 				<path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z" />
 				<path d="M5 3v4" />
 				<path d="M19 17v4" />
