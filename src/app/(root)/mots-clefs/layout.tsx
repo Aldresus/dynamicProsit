118e60756b67c843f78cbadd7fd27a573ccc3d26
Aldresus <aldresusgames@gmail.com>
2023-12-04 11:15:53 +0100

    title/desc for each page

---
+++ src/app/(root)/mots-clefs/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "DynamicPrositX | Mots-Clefs",
+	description:
+		"Une interface intuitive et efficace pour accompagner votre recherche de mots-clefs.",
+};
+export default function MotsClefsLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return <div>{children}</div>;
+}
