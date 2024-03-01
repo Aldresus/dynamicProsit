82fcdfa7c0e468aa8d67f5428c366610750a67f3
Aldresus <hugo.champy@viacesi.fr>
2024-03-01 15:18:35 +0100

    split pres

--- src/app/(seo)/presentation/layout.tsx
+++ src/app/(seo)/presentation/layout.tsx
@@ -3,45 +3,45 @@
 import { MantineProvider, createTheme } from "@mantine/core";
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	const theme = createTheme({
-		fontSizes: {
-			xs: "1.5rem",
-			sm: "1.6rem",
-			md: "1.75rem",
-			lg: "1.85rem",
-			xl: "2rem",
-		},
-		headings: {
-			sizes: {
-				h1: {
-					fontSize: "3rem",
-				},
-				h2: {
-					fontSize: "2.5rem",
-				},
-				h3: {
-					fontSize: "2rem",
-				},
-				h4: {
-					fontSize: "1.75rem",
-				},
-				h5: {
-					fontSize: "1.5rem",
-				},
-				h6: {
-					fontSize: "1.25rem",
-				},
-			},
-		},
-	});
+  const theme = createTheme({
+    fontSizes: {
+      xs: "1.5rem",
+      sm: "1.6rem",
+      md: "1.75rem",
+      lg: "1.85rem",
+      xl: "2rem",
+    },
+    headings: {
+      sizes: {
+        h1: {
+          fontSize: "3rem",
+        },
+        h2: {
+          fontSize: "2.5rem",
+        },
+        h3: {
+          fontSize: "2rem",
+        },
+        h4: {
+          fontSize: "1.75rem",
+        },
+        h5: {
+          fontSize: "1.5rem",
+        },
+        h6: {
+          fontSize: "1.25rem",
+        },
+      },
+    },
+  });
 
-	return (
-		<div className="w-full min-h-screen p-20">
-			<MantineProvider theme={theme}>{children}</MantineProvider>
-		</div>
-	);
+  return (
+    <div className="w-full h-screen px-20 pt-10">
+      <MantineProvider theme={theme}>{children}</MantineProvider>
+    </div>
+  );
 }
--- src/app/(seo)/presentation/page.tsx
+++ src/app/(seo)/presentation/page.tsx
@@ -3,162 +3,71 @@
 import PresentationElement from "@/app/(seo)/presentation/presentationElement";
 import PrositContext, { defaultPrositValue } from "@/components/prositContext";
 import { Prosit } from "@/types/prosit";
-import { Box, Title } from "@mantine/core";
+import { Box, Button, Group, Stack, Title } from "@mantine/core";
 import React, { useContext, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import Presentation from "./presentation";
+import { Plus } from "lucide-react";
 
 export default function Visualisation() {
-	const { prosit } = useContext(PrositContext);
-	const [prositState, setPrositState] = useState<Prosit>(defaultPrositValue);
+  const { prosit } = useContext(PrositContext);
+  const [prositState, setPrositState] = useState<Prosit>(prosit);
+  const [numberOfViews, setNumberOfViews] = useState(1);
 
-	useEffect(() => {
-		setPrositState(prosit);
-	}, [prosit]);
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return;
+  //   window.addEventListener("storage", () => {
+  //     const storedProsit = localStorage.getItem("prosit");
+  //     const parsedProsit = storedProsit
+  //       ? JSON.parse(storedProsit)
+  //       : defaultPrositValue;
+  //   });
+  // }, []);
+  //
+  useEffect(() => {
+    // Function to handle the storage event
+    const handleStorageChange = (event) => {
+      if (event.key === "prosit") {
+        const storedProsit = event.newValue; // Direct access to the updated value
+        const parsedProsit = storedProsit
+          ? JSON.parse(storedProsit)
+          : defaultPrositValue;
+        setPrositState(parsedProsit);
+      }
+    };
 
-	useEffect(() => {
-		if (typeof window === "undefined") return;
-		window.addEventListener("storage", () => {
-			const storedProsit = localStorage.getItem("prosit");
-			const parsedProsit = storedProsit
-				? JSON.parse(storedProsit)
-				: defaultPrositValue;
-			setPrositState(parsedProsit);
-		});
-	}, []);
+    if (typeof window !== "undefined") {
+      window.addEventListener("storage", handleStorageChange);
+    }
 
-	useEffect(() => {
-		if (typeof window !== "undefined")
-			window.document
-				.getElementById(prositState.currentAnchor)
-				?.scrollIntoView({
-					block: "start",
-					behavior: "smooth",
-				});
-	}, [prositState.currentAnchor]);
+    // Cleanup function to remove the event listener
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("storage", handleStorageChange);
+      }
+    };
+  }, []);
 
-	return (
-		<div className="h-full w-full flex flex-col gap-6">
-			<Title order={1}>Prosit : {prositState.titre}</Title>
-			<Box
-				id="informations"
-				className={twMerge("flex flex-col gap-6 rounded-xl")}
-				bg={prositState.currentAnchor === "informations" ? "blue.1" : undefined}
-				c={prositState.currentAnchor === "informations" ? "blue.9" : undefined}
-			>
-				<PresentationElement
-					titre={"Contexte :"}
-					anchor="contexte"
-					valeurs={prositState.contexte}
-					className={twMerge(
-						prositState.contexte.length === 0 ? "opacity-0" : "opacity-100",
-					)}
-				/>
-				<PresentationElement
-					anchor="generalisation"
-					titre={"Generalisation :"}
-					valeurs={prositState.generalisation}
-					className={twMerge(
-						prositState.generalisation.length === 0
-							? "opacity-0"
-							: "opacity-100",
-					)}
-				/>
-			</Box>
-			<div className="flex flex-col gap-9 w-full h-full whitespace-break-spaces overflow-hidden">
-				<PresentationElement
-					anchor="motsClefs"
-					titre={"Mots-clefs"}
-					valeurs={prositState.motsCles}
-					bg={prositState.currentAnchor === "motsClefs" ? "blue.1" : undefined}
-					color={
-						prositState.currentAnchor === "motsClefs" ? "blue.9" : undefined
-					}
-					className={twMerge(
-						prositState.motsCles.length === 0 ? "opacity-0" : "opacity-100",
-					)}
-				/>
-				<PresentationElement
-					anchor="contraintes"
-					titre={"Contraintes"}
-					valeurs={prositState.contraintes}
-					className={twMerge(
-						prositState.contraintes.length === 0 ? "opacity-0" : "opacity-100",
-					)}
-					bg={
-						prositState.currentAnchor === "contraintes" ? "blue.1" : undefined
-					}
-					color={
-						prositState.currentAnchor === "contraintes" ? "blue.9" : undefined
-					}
-				/>
-				<PresentationElement
-					anchor="problematiques"
-					titre={"Problematiques"}
-					valeurs={prositState.problematiques}
-					className={twMerge(
-						prositState.problematiques.length === 0
-							? "opacity-0"
-							: "opacity-100",
-					)}
-					bg={
-						prositState.currentAnchor === "problematiques"
-							? "blue.1"
-							: undefined
-					}
-					color={
-						prositState.currentAnchor === "problematiques"
-							? "blue.9"
-							: undefined
-					}
-				/>
-				<PresentationElement
-					anchor="pistesDeSolution"
-					titre={"Pistes de solution"}
-					valeurs={prositState.pistesDeSolutions}
-					className={twMerge(
-						prositState.pistesDeSolutions.length === 0
-							? "opacity-0"
-							: "opacity-100",
-					)}
-					bg={
-						prositState.currentAnchor === "pistesDeSolution"
-							? "blue.1"
-							: undefined
-					}
-					color={
-						prositState.currentAnchor === "pistesDeSolution"
-							? "blue.9"
-							: undefined
-					}
-				/>
-				<PresentationElement
-					anchor="livrables"
-					titre={"Livrables"}
-					valeurs={prositState.livrables}
-					className={twMerge(
-						prositState.livrables.length === 0 ? "opacity-0" : "opacity-100",
-					)}
-					bg={prositState.currentAnchor === "livrables" ? "blue.1" : undefined}
-					color={
-						prositState.currentAnchor === "livrables" ? "blue.9" : undefined
-					}
-				/>
-				<PresentationElement
-					anchor="planDAction"
-					ordered
-					titre={"Plan d'action :"}
-					valeurs={prositState.planDAction.map((value) => value.content)}
-					className={twMerge(
-						prositState.planDAction.length === 0 ? "opacity-0" : "opacity-100",
-					)}
-					bg={
-						prositState.currentAnchor === "planDAction" ? "blue.1" : undefined
-					}
-					color={
-						prositState.currentAnchor === "planDAction" ? "blue.9" : undefined
-					}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-full w-full flex overflow-hidden">
+      <div className="flex flex-col gap-6 ">
+        <Title order={1}>Prosit : {prositState.titre}</Title>
+
+        <div className="flex gap-2 overflow-hidden">
+          <div
+            className="fixed flex items-center group top-0 bottom-0 right-0 p-3 cursor-pointer"
+            onClick={() => setNumberOfViews((prev) => prev + 1)}
+          >
+            <Button className="h-1/2 w-full translate-x-20 group-hover:translate-x-0 transition-transform">
+              <Plus />
+            </Button>
+          </div>
+
+          {Array.from({ length: numberOfViews }).map((_, index) => (
+            <Presentation prosit={prositState} key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
