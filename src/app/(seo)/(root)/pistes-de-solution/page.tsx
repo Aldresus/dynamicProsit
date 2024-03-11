aa2188f50cb4124f596a940a499719c314a84538
Quentin <quentin.stubecki@viacesi.fr>
2024-03-11 17:40:52 +0100

    input sticky

--- src/app/(seo)/(root)/pistes-de-solution/page.tsx
+++ src/app/(seo)/(root)/pistes-de-solution/page.tsx
@@ -8,129 +8,127 @@ import usePrositPart from "@/hooks/usePrositPart";
 import { AnchorsKeys } from "@/types/anchors";
 import { PrositKeys } from "@/types/prosit";
 import {
-  DndContext,
-  KeyboardSensor,
-  PointerSensor,
-  closestCenter,
-  useSensor,
-  useSensors,
+	DndContext,
+	KeyboardSensor,
+	PointerSensor,
+	closestCenter,
+	useSensor,
+	useSensors,
 } from "@dnd-kit/core";
 import {
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
+	SortableContext,
+	sortableKeyboardCoordinates,
+	verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { Button, Kbd, Text, Textarea, Title } from "@mantine/core";
 import { getHotkeyHandler, useHotkeys } from "@mantine/hooks";
 import React, { useContext, useEffect, useState } from "react";
 
 export default function Pistes() {
-  const { prosit, setProsit } = useContext(PrositContext);
-  const {
-    addItem,
-    deleteItem,
-    editItem,
-    handleDragEnd,
-    items,
-    setWorkingItem,
-    workingItem,
-  } = usePrositPart({
-    prosit: prosit,
-    setProsit,
-    key: PrositKeys.PISTES_DE_SOLUTIONS,
-  });
+	const { prosit, setProsit } = useContext(PrositContext);
+	const {
+		addItem,
+		deleteItem,
+		editItem,
+		handleDragEnd,
+		items,
+		setWorkingItem,
+		workingItem,
+	} = usePrositPart({
+		prosit: prosit,
+		setProsit,
+		key: PrositKeys.PISTES_DE_SOLUTIONS,
+	});
 
-  const { navigate } = useNavigator({ prosit, setProsit });
+	const { navigate } = useNavigator({ prosit, setProsit });
 
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
+	const sensors = useSensors(
+		useSensor(PointerSensor),
+		useSensor(KeyboardSensor, {
+			coordinateGetter: sortableKeyboardCoordinates,
+		}),
+	);
 
-  // biome-ignore lint/suspicious/noExplicitAny: issue with Mantine types
-  const pageHotkeys: any[] = [
-    ["ctrl+enter", () => navigate(AnchorsKeys.LIVRABLES)],
-    ["ctrl+shift+enter", () => navigate(AnchorsKeys.PROBLEMATIQUES)],
-  ];
+	// biome-ignore lint/suspicious/noExplicitAny: issue with Mantine types
+	const pageHotkeys: any[] = [
+		["ctrl+enter", () => navigate(AnchorsKeys.LIVRABLES)],
+		["ctrl+shift+enter", () => navigate(AnchorsKeys.PROBLEMATIQUES)],
+	];
 
-  useHotkeys(pageHotkeys);
+	useHotkeys(pageHotkeys);
 
-  const hotkeys = getHotkeyHandler([
-    ["enter", () => addItem()],
-    ["shift+enter", () => addItem()],
-    ...globalHotKeys(navigate),
-    ...pageHotkeys,
-  ]);
+	const hotkeys = getHotkeyHandler([
+		["enter", () => addItem()],
+		["shift+enter", () => addItem()],
+		...globalHotKeys(navigate),
+		...pageHotkeys,
+	]);
 
-  return (
-    <div className="h-full flex flex-col gap-9 py-3">
-      <Title order={2}>Pistes de solution</Title>
+	return (
+		<div className="h-full flex flex-col gap-9 py-3">
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					addItem();
+				}}
+				className="flex flex-col sticky gap-2 z-10 top-0 p-2 pt-4 bg-white rounded-md w-full"
+			>
+				<Textarea
+					onKeyDown={hotkeys}
+					className="flex-1"
+					autoFocus
+					w="100%"
+					label={<Title order={2}>Pistes de solution</Title>}
+					placeholder="Il faut faire un algorithme de recherche de fromage avec python"
+					value={workingItem.content}
+					onInput={(event) => {
+						setWorkingItem({
+							...workingItem,
+							content: event.currentTarget.value,
+						});
+					}}
+				/>
+				<div className="w-full flex justify-between items-start">
+					<div className="flex flex-col justify-between">
+						<Text c="dimmed">
+							<Kbd>double clic</Kbd> pour éditer la ligne
+						</Text>
+					</div>
+					<Button
+						w="40%"
+						type="submit"
+						onClick={(event) => {
+							event.preventDefault();
+							addItem();
+						}}
+					>
+						Ajouter la piste
+					</Button>
+				</div>
+			</form>
 
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          addItem();
-        }}
-        className="flex flex-col gap-2 items-end"
-      >
-        <Textarea
-          onKeyDown={hotkeys}
-          className="flex-1"
-          autoFocus
-          w="100%"
-          label="Piste de solution"
-          placeholder="Il faut faire un algorithme de recherche de fromage avec python"
-          value={workingItem.content}
-          onInput={(event) => {
-            setWorkingItem({
-              ...workingItem,
-              content: event.currentTarget.value,
-            });
-          }}
-        />
-        <div className="w-full flex justify-between items-start">
-          <div className="flex flex-col justify-between">
-            <Text c="dimmed">
-              <Kbd>double clic</Kbd> pour éditer la ligne
-            </Text>
-          </div>
-          <Button
-            w="40%"
-            type="submit"
-            onClick={(event) => {
-              event.preventDefault();
-              addItem();
-            }}
-          >
-            Ajouter la piste
-          </Button>
-        </div>
-      </form>
-
-      <DndContext
-        sensors={sensors}
-        collisionDetection={closestCenter}
-        onDragEnd={handleDragEnd}
-      >
-        <SortableContext
-          items={items.map((item) => item.id)}
-          strategy={verticalListSortingStrategy}
-        >
-          <div className="w-full flex flex-col">
-            {/*todo avoir des sous truc jor 1. a. b. c. */}
-            {items.map((value) => (
-              <SortableItem
-                key={value.id}
-                value={value}
-                deleteItem={() => deleteItem(value.id)}
-                editItem={(newValue: string) => editItem(newValue, value.id)}
-              />
-            ))}
-          </div>
-        </SortableContext>
-      </DndContext>
-    </div>
-  );
+			<DndContext
+				sensors={sensors}
+				collisionDetection={closestCenter}
+				onDragEnd={handleDragEnd}
+			>
+				<SortableContext
+					items={items.map((item) => item.id)}
+					strategy={verticalListSortingStrategy}
+				>
+					<div className="w-full flex flex-col">
+						{/*todo avoir des sous truc jor 1. a. b. c. */}
+						{items.map((value) => (
+							<SortableItem
+								key={value.id}
+								value={value}
+								deleteItem={() => deleteItem(value.id)}
+								editItem={(newValue: string) => editItem(newValue, value.id)}
+							/>
+						))}
+					</div>
+				</SortableContext>
+			</DndContext>
+		</div>
+	);
 }
--- src/components/sortableItem.tsx
+++ src/components/sortableItem.tsx
@@ -10,115 +10,113 @@ import { GripVertical, Minus, Trash } from "lucide-react";
 import React from "react";
 
 interface SortableItemProps extends React.HTMLAttributes<HTMLDivElement> {
-  value: OrderedItem;
-  index?: number;
-  editItem: (newValue: string) => void;
-  deleteItem: (id: string) => void;
-  undraggable?: boolean;
+	value: OrderedItem;
+	index?: number;
+	editItem: (newValue: string) => void;
+	deleteItem: (id: string) => void;
+	undraggable?: boolean;
 }
 
 export default function SortableItem({
-  deleteItem,
-  editItem,
-  value,
-  index,
-  undraggable = false,
-  ...props
+	deleteItem,
+	editItem,
+	value,
+	index,
+	undraggable = false,
+	...props
 }: SortableItemProps) {
-  const [editMode, setEditMode] = React.useState(false);
-  const [editValue, setEditValue] = React.useState(value.content);
+	const [editMode, setEditMode] = React.useState(false);
+	const [editValue, setEditValue] = React.useState(value.content);
 
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: value.id });
+	const { attributes, listeners, setNodeRef, transform, transition } =
+		useSortable({ id: value.id });
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
+	const style = {
+		transform: CSS.Transform.toString(transform),
+		transition,
+	};
 
-  return (
-    <Box
-      className={clsx(
-        "group flex items-center justify-between w-full rounded",
-        {
-          [classes.bgInput]: editMode,
-        },
-      )}
-      ref={setNodeRef}
-      style={style}
-      onDoubleClick={() => {
-        setEditMode(true);
-        editItem(editValue);
-      }}
-    >
-      <div className="flex gap-1 flex-1 items-center">
-        <Group gap={1}>
-          <Text c="blue" className="flex items-center ">
-            {undraggable ? (
-              <Minus size={24} />
-            ) : (
-              <GripVertical
-                className="cursor-move"
-                size={24}
-                {...attributes}
-                {...listeners}
-              />
-            )}
-          </Text>
-          {index !== undefined && (
-            <Title order={3} pr="sm">
-              {index + 1}.
-            </Title>
-          )}
-        </Group>
-        <form
-          className="flex-1"
-          onSubmit={(event) => {
-            event.preventDefault();
-            setEditMode(false);
-            editItem(editValue);
-          }}
-        >
-          {/* <Text  className="flex"> */}
-          <Textarea
-            fw="500"
-            variant="unstyled"
-            p={0}
-            readOnly={!editMode}
-            autosize
-            className={clsx("rounded-md flex-1 focus:outline-none p-0", {
-              [classes.bgInput]: editMode,
-            })}
-            classNames={{
-              input: "p-1 m-0 text-base leading-1",
-              root: "p-0 m-0",
-            }}
-            // biome-ignore lint/a11y/noAutofocus: <explanation>
-            autoFocus
-            value={editValue}
-            onChange={(event) => {
-              setEditValue(event.target.value);
-            }}
-            onBlur={() => {
-              setEditMode(false);
-              editItem(editValue);
-            }}
-          />
-          {/* </Text> */}
-        </form>
+	return (
+		<Box
+			className={clsx(
+				"group flex items-center justify-between w-full rounded",
+				{
+					[classes.bgInput]: editMode,
+				},
+			)}
+			ref={setNodeRef}
+			style={style}
+			onDoubleClick={() => {
+				setEditMode(true);
+				editItem(editValue);
+			}}
+		>
+			<div className="flex gap-1 flex-1 items-center">
+				<Group gap={1}>
+					<Text c="blue" className="flex items-center ">
+						{undraggable ? (
+							<Minus size={24} />
+						) : (
+							<GripVertical
+								className="cursor-move"
+								size={24}
+								{...attributes}
+								{...listeners}
+							/>
+						)}
+					</Text>
+					{index !== undefined && (
+						<Title order={3} pr="sm">
+							{index + 1}.
+						</Title>
+					)}
+				</Group>
+				<form
+					className="flex-1"
+					onSubmit={(event) => {
+						event.preventDefault();
+						setEditMode(false);
+						editItem(editValue);
+					}}
+				>
+					{/* <Text  className="flex"> */}
+					<Textarea
+						fw="500"
+						variant="unstyled"
+						p={0}
+						readOnly={!editMode}
+						autosize
+						className={clsx("rounded-md flex-1 focus:outline-none p-0", {
+							[classes.bgInput]: editMode,
+						})}
+						classNames={{
+							input: "p-1 m-0 text-base leading-1",
+							root: "p-0 m-0",
+						}}
+						// biome-ignore lint/a11y/noAutofocus: <explanation>
+						autoFocus
+						value={editValue}
+						onChange={(event) => {
+							setEditValue(event.target.value);
+						}}
+						onBlur={() => {
+							setEditMode(false);
+							editItem(editValue);
+						}}
+					/>
+					{/* </Text> */}
+				</form>
 
-        <div className="relative w-0 h-full">
-          <ActionIcon
-            variant="subtle"
-            onClick={() => deleteItem(value.id)}
-            color="red"
-            className="opacity-10 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-100"
-          >
-            <Trash size={24} />
-          </ActionIcon>
-        </div>
-      </div>
-      {/* <div className="flex group gap-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-100">
+				<ActionIcon
+					variant="subtle"
+					onClick={() => deleteItem(value.id)}
+					color="red"
+					className="opacity-10 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-100"
+				>
+					<Trash size={24} />
+				</ActionIcon>
+			</div>
+			{/* <div className="flex group gap-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-100">
 				<Button
 					variant="transparent"
 					size="compact-md"
@@ -128,6 +126,6 @@ export default function SortableItem({
 					Supprimer
 				</Button>
 			</div> */}
-    </Box>
-  );
+		</Box>
+	);
 }
